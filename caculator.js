60ccea20cfbdb602188812387a542b5e631c0ab4
stevetanex <113615775+stevetanex@users.noreply.github.com>
2025-08-11 16:23:34 +0530

    v1

---
+++ caculator.js
@@ -0,0 +1,10 @@
+// calculator screen display code
+function todisplay(content){
+    cal.value+=content
+}
+function calclear(){
+    cal.value=""
+}
+function equal(){
+cal.value=eval(cal.value)
+}
